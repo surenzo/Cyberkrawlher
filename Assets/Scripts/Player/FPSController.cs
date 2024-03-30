3dbe8c2ab14c929f856542736710c7dcd47b7c34
Thomien Leygonie <124178882+ToddyTreaks@users.noreply.github.com>
2024-03-30 12:00:18 +0100

    modif animator controler and script FPSControler

--- Assets/Scripts/Player/FPSController.cs
+++ Assets/Scripts/Player/FPSController.cs
@@ -7,6 +7,8 @@ using Cinemachine;
 
 public class FPSController : MonoBehaviour
 {
+    #region Variables
+    
     public float walkingSpeed = 7.5f;
     public float runningSpeed = 11.5f;
     public float jumpSpeed = 8.0f;
@@ -14,53 +16,78 @@ public class FPSController : MonoBehaviour
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
+    
+    float curSpeedX = 0;
+    float curSpeedY = 0;
+    
 
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
     
-    [SerializeField] private Animator characterAnimator;
+    private Animator characterAnimator;
     private HealthSystem _healthSystem;
     
-    [SerializeField] private CinemachineVirtualCamera VirtualCamera;
-    
-
+    [SerializeField] private CinemachineVirtualCamera DeathVirtualCamera;
     [HideInInspector]
     public bool canMove = true;
+    private static readonly int IsDead = Animator.StringToHash("isDead");
+    private static readonly int Speed = Animator.StringToHash("Speed");
 
+    #endregion
+    
+    #region Initialisation
     void Start()
     {
+        // Get Components
         characterController = GetComponent<CharacterController>();
         _healthSystem = GetComponent<HealthSystem>();
+        characterAnimator = GetComponent<Animator>();
         
         // Lock cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
+    
+    #endregion
+
+    #region Updates
 
     void Update()
+    {
+        PlayerMovement();
+        DeathCheck();
+    }
+
+    #endregion
+
+    #region Movement and Rotation
+    private void PlayerMovement()
+    {
+        MoveCalcul();
+        JumpAndGravity();
+        PlayerRotation();
+        Move();
+    }
+
+    private void MoveCalcul()
     {
         // We are grounded, so recalculate move direction based on axes
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
+        
         // Press Left Shift to run
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
+        curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
         
-        // Move Animation
-        if (curSpeedX != 0 || curSpeedY != 0)
-        {
-            float speed = Math.Abs(( moveDirection.x  + moveDirection.z ) / runningSpeed);
-            characterAnimator.SetFloat("Speed",speed);
-        }
-        else
-        {
-            characterAnimator.SetFloat("Speed",0);
-        }
-        
+    }
+    
+    private void JumpAndGravity()
+    {
+        float movementDirectionY = moveDirection.y;
         // Jump
         if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
@@ -78,10 +105,27 @@ public class FPSController : MonoBehaviour
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
-
+    }
+    
+    private void Move()
+    {
         // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
-
+        
+        // Animation
+        if (curSpeedX != 0 || curSpeedY != 0)
+        {
+            float speed = Math.Abs(( moveDirection.x  + moveDirection.z ) / runningSpeed);
+            characterAnimator.SetFloat(Speed,speed);
+        }
+        else
+        {
+            characterAnimator.SetFloat(Speed,0);
+        }
+    }
+    
+    private void PlayerRotation()
+    {
         // Player and Camera rotation
         if (canMove)
         {
@@ -90,28 +134,28 @@ public class FPSController : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
-        
-        // Death
+    }
+    
+    #endregion
+    
+    #region Death
+    
+    private void DeathCheck()
+    {
         if (_healthSystem._isDead)
         {
-            characterAnimator.SetTrigger("isDead");
-            canMove = false;
-            _healthSystem._isDead = false;
             StartCoroutine(Death());
-
         }
     }
     
     IEnumerator Death()
     {
-        VirtualCamera.Priority = 10;
-        var deathTime = 1.9f;
-        var timer = 0f;
-        while (timer < deathTime)
-        {
-            timer += Time.deltaTime;
-            characterController.height -= Time.deltaTime;
-        }
-        yield return new WaitForSeconds(3);
+        characterAnimator.SetTrigger(IsDead);
+        canMove = false;
+        _healthSystem._isDead = false;
+        DeathVirtualCamera.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
     }
+    
+    #endregion
 }
