f7366e0a8fc0c68ae099f9d442c6e2b7df5787bb
Wangha <132218157+Arwangha@users.noreply.github.com>
2024-04-01 22:02:07 +0200

    Working easter eggs version

--- Assets/Scripts/EsterEggs/VendingMachine.cs
+++ Assets/Scripts/EsterEggs/VendingMachine.cs
@@ -4,28 +4,34 @@ using UnityEngine;
 
 public class VendingMachine : MonoBehaviour
 {
-    [Header("Hologram")]
-    [SerializeField] private Mesh hologramMesh;
+    [Header("Hologram")] [SerializeField] private Mesh hologramMesh;
     [SerializeField] private List<GameObject> itemMeshes;
-    [ColorUsage(true,true)] [SerializeField] private Color hologramColor;
-    [ColorUsage(true,true)] [SerializeField] private Color hologramOutlineColor;
-    
-    
-    [Header("Hologram transform settings")]
-    [SerializeField] private Vector3 hologramSize = new Vector3(1, 1, 1);
+
+    [ColorUsage(true, true)] [SerializeField]
+    private Color hologramColor;
+
+    [ColorUsage(true, true)] [SerializeField]
+    private Color hologramOutlineColor;
+
+
+    [Header("Hologram transform settings")] [SerializeField]
+    private Vector3 hologramSize = new Vector3(1, 1, 1);
+
     [SerializeField] private Vector3 hologramRotation = new Vector3(0, 0, 0);
     [SerializeField] private Vector3 hologramPosition = new Vector3(0, 0, 0);
-    
-    [Header("Hologram movement settings")]
-    [SerializeField] private float hologramSpeedRotation = 50f;
+
+    [Header("Hologram movement settings")] [SerializeField]
+    private float hologramSpeedRotation = 50f;
+
     [SerializeField] private float hologramAmplitudeMovement = 0.2f;
     [SerializeField] private float hologramSpeedMovement = 2f;
-    
-    [Header("Chest Settings")]
-    [SerializeField] public bool isOpen = false;
-    
-    [Header("References")]
-    [SerializeField] private GameObject hologram;
+
+    [Header("Chest Settings")] [SerializeField]
+    public bool isOpen = false;
+
+    [Header("References")] [SerializeField]
+    private GameObject hologram;
+
     [SerializeField] private GameObject rayHologram;
     [SerializeField] public EasterEggItem Egg;
 
@@ -43,6 +49,7 @@ public class VendingMachine : MonoBehaviour
     public bool isLooted;
     public int amountChecked;
     private int _prevAmountChecked;
+    private bool _reValidated;
 
     private void OnValidate()
     {
@@ -54,8 +61,8 @@ public class VendingMachine : MonoBehaviour
         hologram.transform.localPosition = hologramPosition;
         hologram.GetComponentInChildren<MeshFilter>().mesh = _itemMesh;
         hologram.GetComponentInChildren<Light>().color = hologramColor.gamma;
-        hologram.GetComponentInChildren<Light>().intensity = hologramColor.maxColorComponent/5;
-        hologram.GetComponentInChildren<MeshRenderer>().sharedMaterial.SetColor("_FresnelColor", hologramOutlineColor); 
+        hologram.GetComponentInChildren<Light>().intensity = hologramColor.maxColorComponent / 5;
+        hologram.GetComponentInChildren<MeshRenderer>().sharedMaterial.SetColor("_FresnelColor", hologramOutlineColor);
         hologram.GetComponentInChildren<MeshRenderer>().sharedMaterial.SetColor("_MainColor", hologramColor);
         var mainModule = rayHologram.GetComponent<ParticleSystem>().main;
         mainModule.startColor = hologramColor;
@@ -69,18 +76,24 @@ public class VendingMachine : MonoBehaviour
         _itemTypes = Enum.GetValues(typeof(EasterEggItem.EasterEggs));
         //_selectedItem = UnityEngine.Random.Range(0, _itemTypes.Length);
         Egg.Egg = (EasterEggItem.EasterEggs)_itemTypes.GetValue(_selectedItem);
-        _itemMesh = itemMeshes[_selectedItem].GetComponent<MeshFilter>().sharedMesh;// /!\ meshes must be in the same order as in the enum
+        _itemMesh = itemMeshes[_selectedItem].GetComponent<MeshFilter>()
+            .sharedMesh; // /!\ meshes must be in the same order as in the enum
         holoScale = itemMeshes[_selectedItem].GetComponent<Transform>().localScale;
         //Debug.Log(holoScale);
-        OnValidate();
-        if (_selectedItem == 0)
-        {
-            hologramMeshObject.transform.Rotate(Vector3.left, 90);
-        }
     }
-    
+
     private void Update()
     {
+        if (!_reValidated)
+        {
+            _reValidated = true;
+            OnValidate();
+            if (_selectedItem == 0)
+            {
+                hologramMeshObject.transform.Rotate(Vector3.left, 90);
+            }
+        }
+
         if (amountChecked >= eggAmount) isLooted = true;
         if (!isOpen || isLooted)
         {
@@ -100,19 +113,21 @@ public class VendingMachine : MonoBehaviour
                 hologramMeshObject.transform.Rotate(Vector3.left, 90);
             }
         }
+
         hologram.transform.localScale = holoScale;
         hologram.transform.Rotate(Vector3.up, hologramSpeedRotation * Time.deltaTime);
         var position = hologram.transform.position;
-        hologram.transform.position = new Vector3(position.x, initialY + Mathf.Sin(Time.time * hologramSpeedMovement) * hologramAmplitudeMovement, position.z);
+        hologram.transform.position = new Vector3(position.x,
+            initialY + Mathf.Sin(Time.time * hologramSpeedMovement) * hologramAmplitudeMovement, position.z);
     }
-    
+
     public void Open()
     {
         isOpen = true;
         hologram.SetActive(true);
         Egg.enabled = true;
     }
-    
+
     public void Close()
     {
         isOpen = false;
